ebfd1f8633bbee1730fca4abee1662e23513a1e0
AnthonyBenes <anthony.benes@huskers.unl.edu>
2019-12-06 11:28:58 -0600

    Added permissions class and base calculations for risks.

---
+++ Mismatchr/Mismatchr/Permission.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+
+namespace Mismatchr
+{
+    public class Permission
+    {
+        public string Name { get; set; }
+        public string[] ProtectionLevel { get; set; }
+
+        public static double getAllRisk(List<Permission> permissions)
+        {
+            double totalRisk = 0;
+            foreach(Permission perm in permissions)
+            {
+                totalRisk += perm.GetRisk();
+            }
+            return totalRisk;
+        }
+
+        public Permission(string line)
+        {
+            string[] split = line.Split(':');
+            Name = split[0];
+            if (split[1] != null)
+            {
+                ProtectionLevel = split[1].Split(',');
+            }
+        }
+
+        public Permission(string name, string level)
+        {
+            Name = name;
+            ProtectionLevel = level.Split(',');
+        }
+
+        public double GetRisk()
+        {
+            double risk = 0;
+            foreach( string level in ProtectionLevel)
+            {
+                switch (level)
+                {
+                    case "dangerous":
+                        risk += 1;
+                        break;
+                    case "signature":
+                        risk += 0.5;
+                        break;
+                    case "normal":
+                        risk += 0.25;
+                        break;
+                    case "priviliged":
+                        risk -= 0.25;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return risk;
+        }
+    }
+}
