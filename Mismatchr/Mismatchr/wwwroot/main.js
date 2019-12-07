4fb6f919696b535771c4e68e98a3629ba5ff5713
Jake Koperski <wijakoperski@gmail.com>
2019-12-06 18:19:44 -0600

    Built frontend

--- Mismatchr/Mismatchr/wwwroot/main.js
+++ Mismatchr/Mismatchr/wwwroot/main.js
@@ -4,6 +4,8 @@
     $.ajax('api/values').done(data => {
         apps = data;
         displayAppList(apps.sort(alphabetically));
+        $('#analysis-preamble').html(`You analyzed <b>${apps.length}</b> apps. Here's what we found...`);
+        displayPermissionUsage(apps);
     });
 
     $('#btn-sort-alphabetically').click(() => {
@@ -17,14 +19,36 @@
     });
 })();
 
+function displayPermissionUsage(apps) {
+    const allUsedPermissions = apps.reduce((p, { usedPermissions }) => [...p, ...usedPermissions.map(u => u.name)], []);
+    const allDeclaredPermissions = apps.reduce((p, { declaredPermissions }) => [...p, ...declaredPermissions.map(d => d.name)], []);
+    const usedPermissionOccurrence = {};
+    const declaredPermissionOccurrence = {};
+
+    allUsedPermissions.forEach(p => {
+        usedPermissionOccurrence[p] = usedPermissionOccurrence[p] ? usedPermissionOccurrence[p] + 1 : 1;
+    });
+
+    allDeclaredPermissions.forEach(p => {
+        declaredPermissionOccurrence[p] = declaredPermissionOccurrence[p] ? declaredPermissionOccurrence[p] + 1 : 1;
+    });
+
+    $('#permission-usage').html(Object.keys(usedPermissionOccurrence).map(key => `${key}: ${usedPermissionOccurrence[key]}<br>`));
+    $('#permission-declarations').append(Object.keys(declaredPermissionOccurrence).map(key => `${key}: ${declaredPermissionOccurrence[key]}<br>`));
+}
+
 function displayAppList(apps) {
-    apps.forEach(({ name, declaredPermissions, usedPermissions }) => {
+    apps.forEach(({ name, score, declaredPermissions, usedPermissions }) => {
+        const isLowRisk = score <= 8;
+        const declaredPermissionsString = declaredPermissions.length > 0 ? declaredPermissions.map(p => p.name).join(', ') : '<i>None declared</i>';
+        const usedPermissionsString = usedPermissions.length > 0 ? usedPermissions.map(p => p.name).join(', ') : '<i>None used</i>';
+
         const element =
-`<div>
-    <div>Application: ${name}</div>
-    <div>Privilege classification: </div>
-    <div>Declared Permissions: ${declaredPermissions.map(p => p.name).join(', ')}</div>
-    <div>Used Permissions: ${usedPermissions.map(p => p.name).join(', ')}</div>
+`<div class='app ${isLowRisk ? 'low-risk' : 'high-risk'}'>
+    <div><b>Application:</b> ${name}</div>
+    <div><b>Risk score:</b> ${score} - ${isLowRisk ? 'LOW RISK' : 'HIGH RISK'}</div>
+    <div><b>Declared Permissions:</b> ${declaredPermissionsString}</div>
+    <div><b>Used Permissions:</b> ${usedPermissionsString}</div>
 </div>`;
 
         $('#app-list').append(element);
@@ -36,9 +60,9 @@ function clearAppList() {
 }
 
 function byRisk(firstApp, secondApp) {
-    if (firstApp.usedPermissions.length > secondApp.usedPermissions.length) {
+    if (firstApp.score > secondApp.score) {
         return -1;
-    } else if (firstApp.usedPermissions.length < secondApp.usedPermissions.length) {
+    } else if (firstApp.score < secondApp.score) {
         return 1;
     }
 
